5cc6d5a31fab059c9751b2abfa49b71e0f6ca8b8
shadowhacker8 <happyemilliano@gmail.com>
2025-09-24 04:12:13 +0100

    hhg

--- next.config.ts
+++ next.config.ts
@@ -7,16 +7,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
 }
 
 export default nextConfig
